4060d97f034e46b348d43b1f02df88efdb5a1a79
json520 <json4399@163.com>
2017-08-19 12:23:36 +0800

    重新完成banner滚动

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,1 +1,19 @@
-// 提交
+// 添加class
+export function addClass(el,className){
+    if(hasClass(el,className)){
+        return 
+    }else{
+        
+        let newClass = el.className.split(' ');  //分割成数组
+        newClass.push(className); //把新的clasName添加进去
+        el.className = newClass.join(' '); //拼接后赋值给元素  
+    }
+
+}
+
+// 判断在这个dom里面有没有class
+export function hasClass(el,className){
+    // 设置一个正则，因为el.className是字符串所以可以用test进行正则验证
+    let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+    return reg.test(el.className)
+}
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="hello">
-    <ul>
-    	<li v-for="(item,index) in bannerList">
-        <img :src="item.picUrl" alt="">
-      </li>
-    </ul>
+  <div class="recommend">
+   <div>
+     <div v-if="recommendList.length" class="slider_wrapper">
+        <v-slider>
+            <div v-for="(item,index) in recommendList" :key="index">
+                <a :href="item.linkUrl">
+                  <img :src="item.picUrl" />
+                </a>               
+            </div>
+        </v-slider>
+     </div>
+   </div>
     
   </div>
 </template>
@@ -12,14 +18,17 @@
 <script>
 import { getRecommend } from '@/api/recommend'
 import { ERR_ok } from '@/api/config'
-
+import VSlider from '@/base/slider/slider'
 export default {
-  name: 'hello',
+  name: 'recommend',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      bannerList:[]
+      recommendList:[]
     }
+  },
+  components:{
+    VSlider
   },
   methods:{
     getAjaxData(){
@@ -28,7 +37,7 @@ export default {
         console.log(res)
         if(res.code === ERR_ok){
           console.log('成功返回')
-          this.bannerList = res.data.slider;
+          this.recommendList = res.data.slider;
         }
       }).catch((err) =>{
         console.log(err)
@@ -44,21 +53,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
